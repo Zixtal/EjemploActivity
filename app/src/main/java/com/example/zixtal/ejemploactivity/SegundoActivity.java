42590bf830e0beff1ec4dbaa6298ac199fc7a69f
zixtal <sabritox@gmail.com>
2015-09-08 20:37:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/zixtal/ejemploactivity/SegundoActivity.java
@@ -0,0 +1,16 @@
+package com.example.zixtal.ejemploactivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Zixtal on 08/09/2015.
+ */
+public class SegundoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.segunda_activity);
+    }
+}
